89d6bb29f752c60bd6a6a0e7ba46feaca435bf7c
anacondaf <nkai.dev@gmail.com>
2024-06-07 23:22:08 +0700

    Comment out EventHandler

---
+++ todoapp.backend/API/Infrastructure/Sidecar/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Sidecar.KeyVault;
+
+namespace Infrastructure.Sidecar.KeyVault;
+
+public static class SidecarExtensions
+{
+    public static IServiceCollection UseKVConfiguration(this IServiceCollection services, IConfiguration configuration)
+    {
+        var keyVaultSettings = configuration.GetSection(nameof(KeyVaultSettings)).Get<KeyVaultSettings>();
+
+        if (keyVaultSettings != null && !string.IsNullOrEmpty(keyVaultSettings.Uri))
+        {
+            if (!string.IsNullOrEmpty(keyVaultSettings.ClientId) && !string.IsNullOrEmpty(keyVaultSettings.ClientSecret))
+            {
+                ClientSecretCredential clientCred = new ClientSecretCredential(keyVaultSettings.TenantId, keyVaultSettings.ClientId, keyVaultSettings.ClientSecret);
+
+                var client = new SecretClient(new Uri(keyVaultSettings.Uri), clientCred);
+                services.AddSingleton(client);
+            }
+        }
+
+        return services;
+    }
+}
